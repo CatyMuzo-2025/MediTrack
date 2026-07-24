262534b9efa080ee34fcf3c3db00cd37a253f148
CatyMuzo-2025 <crmuzo@espe.edu.ec>
2026-07-24 13:03:51 -0500

    fix: agrega excepcion AppointmentNotFoundException para citas no encontradas

--- src/main/java/com/espe/meditrack/service/AppointmentNotFoundException.java
+++ src/main/java/com/espe/meditrack/service/AppointmentNotFoundException.java
@@ -1,4 +1,8 @@
 package com.espe.meditrack.service;
 
-public class AppointmentNotFoundException {
+public class AppointmentNotFoundException extends RuntimeException {
+
+    public AppointmentNotFoundException(String id) {
+        super("No se encontro la cita con id: " + id);
+    }
 }
